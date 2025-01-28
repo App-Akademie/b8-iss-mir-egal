d146ad4338f59fb3951f817543b73c09a8cda3ef
Max Schwarzkopf <max_schwarzkopf@gmx.de>
2025-01-28 09:54:26 +0100

    chore: add db repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:iss_mir_egal/home/data/database_repository.dart';
+import 'package:iss_mir_egal/home/data/mock_database.dart';
 import 'package:iss_mir_egal/my_app.dart';
 
 void main() {
+  DatabaseRepository mockDatabase = MockDatabase();
+
   runApp(const MyApp());
 }
