7183ca37472f7ae8e1b8fabf016cdf64334bfe94
Max Schwarzkopf <max_schwarzkopf@gmx.de>
2025-01-29 09:56:19 +0100

    feat: add raw login page

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:iss_mir_egal/auth/view/login_page.dart';
 import 'package:iss_mir_egal/home/models/meal.dart';
-import 'package:iss_mir_egal/home/screens/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({required this.meals, super.key});
@@ -10,9 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(
-        meals: meals,
-      ),
+      home: LoginPage(),
     );
   }
 }
